8e7def633518df09fcf7a1ca7b262c2b2658e8c7
Ahmed Fahmy <31390580+Ahmed-Fahmy10@users.noreply.github.com>
2020-05-31 12:39:52 +0200

    Update CCrystalEditView.h

--- CCrystalEditView.h
+++ CCrystalEditView.h
@@ -1,20 +1,3 @@
-////////////////////////////////////////////////////////////////////////////
-//	File:		CCrystalEditView.h
-//	Version:	1.0.0.0
-//	Created:	29-Dec-1998
-//
-//	Author:		Stcherbatchenko Andrei
-//	E-mail:		windfall@gmx.de
-//
-//	Interface of the CCrystalEditView class, a part of Crystal Edit - syntax
-//	coloring text editor.
-//
-//	You are free to use or modify this code to the following restrictions:
-//	- Acknowledge me somewhere in your about box, simple "Parts of code by.."
-//	will be enough. If you can't (or don't want to), contact me personally.
-//	- LEAVE THIS HEADER INTACT
-////////////////////////////////////////////////////////////////////////////
-
 #if !defined(AFX_CCRYSTALEDITVIEW_H__8F3F8B63_6F66_11D2_8C34_0080ADB86836__INCLUDED_)
 #define AFX_CCRYSTALEDITVIEW_H__8F3F8B63_6F66_11D2_8C34_0080ADB86836__INCLUDED_
 
