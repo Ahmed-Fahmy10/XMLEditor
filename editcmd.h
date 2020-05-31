eb5bbb10a4a16101173df49c9b1176eddf48e695
Ahmed Fahmy <31390580+Ahmed-Fahmy10@users.noreply.github.com>
2020-05-31 12:50:11 +0200

    Update editcmd.h

--- editcmd.h
+++ editcmd.h
@@ -1,21 +1,3 @@
-////////////////////////////////////////////////////////////////////////////
-//	File:		editcmd.h
-//	Version:	1.0.0.0
-//	Created:	29-Dec-1998
-//
-//	Author:		Stcherbatchenko Andrei
-//	E-mail:		windfall@gmx.de
-//
-//	Command defintions and resource IDs for Crystal Edit - syntax coloring
-//	text editor.
-//	Include this in .RC file in your project.
-//
-//	You are free to use or modify this code to the following restrictions:
-//	- Acknowledge me somewhere in your about box, simple "Parts of code by.."
-//	will be enough. If you can't (or don't want to), contact me personally.
-//	- LEAVE THIS HEADER INTACT
-////////////////////////////////////////////////////////////////////////////
-
 #ifndef _EDITCMD_H_INCLUDED
 #define _EDITCMD_H_INCLUDED
 
