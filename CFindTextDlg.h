93396044b50b9b5354b11c810c94bd1f5469564a
Ahmed Fahmy <31390580+Ahmed-Fahmy10@users.noreply.github.com>
2020-05-31 12:43:04 +0200

    Update CFindTextDlg.h

--- CFindTextDlg.h
+++ CFindTextDlg.h
@@ -1,20 +1,3 @@
-////////////////////////////////////////////////////////////////////////////
-//	File:		CFindTextDlg.h
-//	Version:	1.0.0.0
-//	Created:	29-Dec-1998
-//
-//	Author:		Stcherbatchenko Andrei
-//	E-mail:		windfall@gmx.de
-//
-//	Declaration of the CFindTextDlg dialog, a part of Crystal Edit -
-//	syntax coloring text editor.
-//
-//	You are free to use or modify this code to the following restrictions:
-//	- Acknowledge me somewhere in your about box, simple "Parts of code by.."
-//	will be enough. If you can't (or don't want to), contact me personally.
-//	- LEAVE THIS HEADER INTACT
-////////////////////////////////////////////////////////////////////////////
-
 #if !defined(AFX_CFINDTEXTDLG_H__F59009E3_7B01_11D2_8C4F_0080ADB86836__INCLUDED_)
 #define AFX_CFINDTEXTDLG_H__F59009E3_7B01_11D2_8C4F_0080ADB86836__INCLUDED_
 
