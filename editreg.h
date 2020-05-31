61e905a58f67d9904a7fcc27487ef2e7bee577a8
Ahmed Fahmy <31390580+Ahmed-Fahmy10@users.noreply.github.com>
2020-05-31 12:50:41 +0200

    Update editreg.h

--- editreg.h
+++ editreg.h
@@ -1,19 +1,3 @@
-////////////////////////////////////////////////////////////////////////////
-//	File:		editcmd.h
-//	Version:	1.0.0.0
-//	Created:	29-Dec-1998
-//
-//	Author:		Stcherbatchenko Andrei
-//	E-mail:		windfall@gmx.de
-//
-//	Registry keys and values for Crystal Edit - syntax colorig text editor.
-//
-//	You are free to use or modify this code to the following restrictions:
-//	- Acknowledge me somewhere in your about box, simple "Parts of code by.."
-//	will be enough. If you can't (or don't want to), contact me personally.
-//	- LEAVE THIS HEADER INTACT
-////////////////////////////////////////////////////////////////////////////
-
 #ifndef CRYSEDITREG_H__INCLUDED
 #define CRYSEDITREG_H__INCLUDED
 
